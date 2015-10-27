a93b7c7b90daadb198bac39e72847a5d7e32241c
Gabriel <developer.gabriel@gmail.com>
2015-10-27 18:38:57 -0200

    Exercicios 8.6

--- produto.php
+++ produto.php
@@ -7,6 +7,7 @@
         <link rel="stylesheet" href="css/reset.css">
         <link rel="stylesheet" href="css/estilos.css">
         <link rel="stylesheet" href="css/mobile.css" media="(max-width: 939px)">
+        <link rel="stylesheet" href="css/produto.css">
     </head>
     <body>
         <?php include("cabecalho.php"); ?>
